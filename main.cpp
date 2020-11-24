4417d78a274901e8eca82d1617b3598dfc007e28
Tekena Solomon <tekena157@gmail.com>
2020-11-24 17:24:45 +0100

    Finished game

--- main.cpp
+++ main.cpp
@@ -5,17 +5,35 @@
 
 using namespace std;
 
-int main() {
-  // Generate unique random numbers using current time.
+// Get random number btw 0 and 99
+int getRandomNum()
+{
   srand(time(NULL));
-  // Get random number btw 0 and 99
   unsigned int numberToGuess = rand() % 100;
-  cout << "Guess a number btw 0 and 99 " << endl;
+  return numberToGuess;
+}
 
-  unsigned int playersNumber{};
-  cin >> playersNumber;
-
-  cout << "You guessed: " << playersNumber
-       << "\nThe actual number was: " << numberToGuess << endl;
+int main()
+{
+  int randNum = getRandomNum();
+  int playersNumber{};
+  int tries = 5;
+  while (tries > 0) {
+    cout << "Guess a number btw 0 and 99: ";
+    cin >> playersNumber;
+    if(playersNumber == randNum){
+      cout << "Congratulations!!! You guessed right " << randNum << endl;
+      return 0;
+    }
+    tries -= 1;
+    if(tries > 0){
+      cout << "Wrong guess, You have " << tries << " tries left,\n";
+      playersNumber > randNum ? cout << "Your guess was higher.. " : cout << "Your guess was lower..";
+      cout << endl;
+    }
+    else {
+      cout << "Failure!!! The secret number is: " << randNum << endl;
+    }
+  }
   return 0;
 }
